6d19290cd73e8b03d52aed6b0635f7242d2d6429
Daniel Corcoran <danielc.92@outlook.com>
2019-05-24 18:52:20 +1000

    Added some variables

---
+++ variables.js
@@ -0,0 +1,24 @@
+// let
+let score = 20;
+console.log(score);
+
+// const
+const always_the_same = 'Daniel is learning programming';
+console.log(always_the_same);
+
+// var
+var globally_available = 'PROD_ENV';
+
+// Strings
+let name = "Daniel";
+let name2 = 'George';
+
+// Template strings
+let name_sentence = `my name is ${name} and my score is ${score}`;
+console.log(name_sentence);
+
+// Boolean
+let active = true;
+let inactive = false;
+
+//
